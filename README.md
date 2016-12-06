ecf06ac8ed4511ad5da7100f28916dd908a356fe
Millie Walsh <millie@primeacademy.io>
2016-12-06 13:30:34 -0600

    serve index.html

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 0. create file structure
 1. npm init
 2. npm install express --save
+
+-- Req from browser --
+http://localhost:3000/
+http://10.100.100.55:3000/
+
+file:///Users/millie/prime/tau/tier2/node-express/public/index.html
--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var path = require('path'); //no npm install
 var app = express();
 
+// base route
+app.get('/', function(req, res) {
+  // send back our index.html file
+  console.log('index path -> ', path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000');
